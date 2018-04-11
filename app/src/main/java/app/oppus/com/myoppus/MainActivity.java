53afbe0ad1f13857808905c6ad35f1b5398b49ee
shambo001 <shambo.hore88@gmail.com>
2018-04-11 23:13:22 +0530

    ServicelistAdapter.java have one error

---
+++ app/src/main/java/app/oppus/com/myoppus/MainActivity.java
@@ -0,0 +1,18 @@
+package app.oppus.com.myoppus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] values = {"Aya","Nursing Sister","GNM Nurse","Physiotherapist","House cleaner"};
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/app/oppus/com/myoppus/ServicelistAdapter.java
@@ -0,0 +1,11 @@
+package app.oppus.com.myoppus;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by acer on 10-04-2018.
+ */
+
+public class ServicelistAdapter extends RecyclerView.Adapter<RecyclerView.Adapter> {
+
+}
